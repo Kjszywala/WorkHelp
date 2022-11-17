140e3d639aa7bf2cb6c45ae6fda2d198731a1105
Kamil Szywala <kjszywala@protonmail.com>
2022-11-17 13:15:49 +0000

    Update USBReadWrite.cs

--- USBReadWrite.cs
+++ USBReadWrite.cs
@@ -1,34 +1,96 @@
 using LibUsbDotNet;
 using LibUsbDotNet.LibUsb;
 using LibUsbDotNet.Main;
+using System.Runtime.InteropServices;
+using System.Text;
 
-using (var context = new UsbContext())
+namespace Trial2
 {
-    context.SetDebugLevel(LogLevel.Info);
+    public class USBReadWrite
+    {
+        #region Variables
+        public string PrinterName { get; set; }
+        public Int32 _ProductId { get; set; }
+        public Int32 _VendorId { get; set; }
+        #endregion
 
-    var usbDeviceCollection = context.List();
+        #region Constructor
+        public USBReadWrite(string printerName)
+        {
+            PrinterName = printerName;
+        }
+        #endregion
 
-    var switchUsbDevice = usbDeviceCollection.FirstOrDefault(d => d.ProductId == 0x0001 && d.VendorId == 0x0C1F);
+        #region Helper
+        [DllImport("Libraries/CRC32.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint calculateCRC32(byte[] buffer, int length);
 
-    if (switchUsbDevice != null)
-        switchUsbDevice.Open();
-    else
-        return;
+        public void WriteRead(string setting)
+        {
+            using (var context = new UsbContext())
+            {
+                context.SetDebugLevel(LogLevel.Info);
 
-    switchUsbDevice.ClaimInterface(switchUsbDevice.Configs[0].Interfaces[0].Number);
+                var usbDeviceCollection = context.List();
 
-    var writeEndpoint = switchUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
-    var readEnpoint = switchUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+                var switchUsbDevice = usbDeviceCollection.FirstOrDefault(d => d.ProductId == 0x0001 && d.VendorId == 0x0C1F);
 
-    var buffer = File.ReadAllBytes("C:/Users/kamil.szywala/Desktop/2.prn");
+                if (switchUsbDevice != null)
+                    switchUsbDevice.Open();
+                else
+                    return;
 
-    writeEndpoint.Write(buffer, 3000, out var bytesWritten);
+                switchUsbDevice.ClaimInterface(switchUsbDevice.Configs[0].Interfaces[0].Number);
 
-    var readBuffer = new byte[1024];
+                var writeEndpoint = switchUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
+                var readEnpoint = switchUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
 
-    readEnpoint.Read(readBuffer, 3000, out var readBytes);
+                var Header =
+                 Encoding.UTF8.GetBytes(
+                     "<?xml version=\"1.0\"encoding=\"UTF-8\"?>" +
+                     "<prn>" +
+                     "<binary-size>0</binary-size>" +
+                     "<get>" +
+                     $"<param id=\"{setting}\"/>" +
+                     "</get>" +
+                     "</prn>" +
+                     "\0");
 
-    var b = System.Text.Encoding.UTF8.GetString(readBuffer);
+                var crc32 = BitConverter.GetBytes(calculateCRC32(Header, Header.Length));
 
-    Console.WriteLine(b);
+                var buffer = Header.Concat(crc32).ToArray();
+
+                writeEndpoint.Write(buffer, 3000, out var bytesWritten);
+
+                var readBuffer = new byte[1024];
+
+                readEnpoint.Read(readBuffer, 3000, out var readBytes);
+
+                var b = System.Text.Encoding.UTF8.GetString(readBuffer);
+
+                var value = readValue(b, setting);
+
+                Console.WriteLine(value);
+            }
+        }
+
+        public string readValue(string xml, string setting)
+        {
+            string[] lines = xml.Split('\n');
+
+            string value = "";
+
+            foreach (string line in lines)
+            {
+                line.Trim();
+                if (line.Contains($"{setting}"))
+                {
+                    var source = line.Split('>', '<');
+                    value = source[2];
+                }
+            }
+            return value;
+        }
+        #endregion
+    }
 }
